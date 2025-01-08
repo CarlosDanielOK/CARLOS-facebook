006ee48ae875bc7c7d91995d25e35c1769ecb0d6
Dan <carlosdaniellazo06@gmail.com>
2025-01-08 04:59:49 -0300

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 export default function CarlosFacebook() {
   return (
-    <div>Hola, voy a crear el inicio de Facebook</div>
+    <main>
+      <div>
+        <section>
+          <Image src="/facebook.png" width={400} height={400} alt="facebook" />
+          <h1>Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.</h1>
+        </section>
+        <section>
+          <form>
+            <input type="text" />
+          </form>
+        </section>
+      </div>
+    </main>
   );
 }
