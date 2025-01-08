84b6fdbcc9aba681c43eaf72936dc2ac3d7af947
Dan <carlosdaniellazo06@gmail.com>
2025-01-08 05:08:28 -0300

    iniciando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import Image from "next/image";
-
 export default function CarlosFacebook() {
   return (
-    <main>
-      <div>
-        <section>
-          <Image src="/facebook.png" width={400} height={400} alt="facebook" />
-          <h1>Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.</h1>
-        </section>
-        <section>
-          <form>
-            <input type="text" />
-          </form>
-        </section>
-      </div>
-    </main>
+    <div>Hola, voy a crear el inicio de Facebook</div>
   );
 }
